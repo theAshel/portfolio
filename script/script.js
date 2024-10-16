e8dbe801ce08b57bcf1582294eaf8c4422b345ce
Hoang Long PHAM <hoang-long.pham1@etudiant.univ-reims.fr>
2024-10-16 21:46:40 +0200

    CSS : header + burger menu done

--- script/script.js
+++ script/script.js
@@ -1,9 +1,14 @@
 // Récupération des éléments HTML
 const burger = document.getElementById('burger');
-const menu = document.getElementById('menu');
+const menu = document.getElementById('menu_nav');
+const xmark = document.getElementById('xmark-close-menu')
 
-// Ajout de l'événement pour afficher ou masquer le menu au clic sur le bouton burger
+// Ajout de l'événement pour afficher le menu au clic sur le bouton burger
 burger.addEventListener('click', () => {
-    console.log('caca')
+    menu.classList.toggle('active');
+});
+
+// Ajout de l'événement pour fermer le menu au clic sur la croix
+xmark.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
